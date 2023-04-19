f5a7a4834ea13d439ce8ca506fd0b90b62e97039
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-04-20 02:30:19 +0530

    code in different component of react

---
+++ src/ReactBenefits/ReactBenefits.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+export function Reactbenefits() {
+    return (
+        <Fragment>
+            <h3>Benifits of using react ... </h3>
+            <ol>
+                <li>Component based architecture</li>
+                <li>Virtual Dom for efficient updates</li>
+                <li>Rich ecoSystem and community</li>
+                <li>Cross -platform development</li>
+                <li>Strong community support:</li>
+
+            </ol>
+        </Fragment>
+
+    )
+
+
+}
---
+++ src/Reactlogo/Reactlogo.js
@@ -0,0 +1,6 @@
+import logo from '../logo.svg';
+export function Reactlogo(){
+    return (
+        <img src={logo} className="App-logo" alt="logo" />
+    )
+}
